72605b6299efc9922a14dc3432a3df38aaecaeb6
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-08-26 20:59:58 -0500

    start of 14.2

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +15,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
