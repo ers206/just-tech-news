3b74b2d6d5f3248d32dee79abdc71aa8fbd9c50e
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-08-28 10:03:47 -0500

    end 14.3

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,11 +28,14 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
+  .then(dbPostData => {
+    const posts = dbPostData.map(post => post.get({ plain: true }));
 
-      res.render('homepage', { posts });
-    })
+    res.render('homepage', {
+      posts,
+      loggedIn: req.session.loggedIn
+    });
+  })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -85,7 +88,10 @@ router.get('/post/:id', (req, res) => {
         const post = dbPostData.get({ plain: true });
   
         // pass data to template
-        res.render('single-post', { post });
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+          });
       })
       .catch(err => {
         console.log(err);
